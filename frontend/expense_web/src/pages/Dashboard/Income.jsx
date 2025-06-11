8c2518eff90f0f62edc28923615bca0790f7753e
Nguyễn Khánh <150260593+khnhk0ogei04@users.noreply.github.com>
2025-06-12 02:45:58 +0700

    Update Income.jsx

--- frontend/expense_web/src/pages/Dashboard/Income.jsx
+++ frontend/expense_web/src/pages/Dashboard/Income.jsx
@@ -7,7 +7,6 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import IncomeOverview from "../../components/Income/IncomeOverview";
 import IncomeList from "../../components/Income/IncomeList";
-import AddIncomeModal from "../../components/Income/AddIncomeForm";
 import Modal from "../../components/Modal";
 import AddIncomeForm from "../../components/Income/AddIncomeForm";
 import TransactionFilter from "../../components/TransactionFilter";
